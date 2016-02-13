d6232915e8deb432825c7a7a068c6093a369c4d8
root <root@localhost.localdomain>
2016-02-13 09:32:47 -0600

    yorumlama modulu eklendi

--- blog/models.py
+++ blog/models.py
@@ -12,10 +12,31 @@ class Post(models.Model):
             blank=True, null=True)
 
     def yayinla(self):
-        self. yayinlama_tarihi = timezone.now()
+        print(self)
+        self.yayinlanma_tarihi = timezone.now()
+        print(self)
+        print(self.yayinlanma_tarihi)
         self.save()
 
     def __str__(self):
         return self.baslik
 
+    def approved_comments(self):
+    	return self.comments.filter(approved_comment=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+  
 # Create your models here.
